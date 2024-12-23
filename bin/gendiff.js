c25c4d3129d3b01fcb0462bfeb43ee29588b06a3
Elflight <Elflight@yandex.ru>
2024-12-23 22:51:57 +0300

    first step

---
+++ bin/gendiff.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+
+const genDiff = (filepath1, filepath2) => {return true};
+
+program
+  .name('genDiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0');
+
+// program.command('genDiff')
+//   .description('Compares two configuration files and shows a difference.')
+//   .argument('<filepath1>', 'первый файл')
+//   .argument('<filepath2>', 'второй файл')
+// //   .option('-c, --connector <type>', 'соединительная строка', '')
+//   .action((first, second, options) => {
+//     // BEGIN (write your solution here)
+    
+//     // END
+//   });
+
+program.parse();
